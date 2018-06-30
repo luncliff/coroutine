3ba70a3a1fc2219ef41425aba0dd1eaf41b9a5d7
Park Dong-Ha <luncliff@gmail.com>
2018-06-30 17:36:36 +0000

    Merge 'support/clang'
     - Going to update AppVeyor YAML again

--- magic/coroutine.hpp
+++ magic/coroutine.hpp
@@ -18,7 +18,8 @@
 #include <experimental/generator>
 #endif
 
-namespace magic {
+namespace magic
+{
 namespace stdex = std::experimental;
 
 // - Note
@@ -31,35 +32,38 @@ namespace stdex = std::experimental;
 //      from the resumable function frame.
 //
 //      This type is an alternative of the `std::future<void>`
-class unplug {
+class unplug
+{
 public:
-  struct promise_type {
+  struct promise_type
+  {
     // No suspend for init/final suspension point
     auto initial_suspend() const noexcept { return stdex::suspend_never{}; }
     auto final_suspend() const noexcept { return stdex::suspend_never{}; }
     // Ignore return of the coroutine
-    void return_void(void) noexcept {};
-    void unhandled_exception() noexcept{};
+    void return_void(void) noexcept {}
+    void unhandled_exception() noexcept {}
 
     promise_type &get_return_object() noexcept { return *this; }
 
     // - Note
     //      Examples for memory management customization.
-    // void *operator new(size_t _size)
-    //{
-    //    return __mgmt.allocate(_size);
-    //}
+    // void *operator new(size_t _size) noexcept(false)
+    // {
+    //   //  return __mgmt.allocate(_size);
+    //   return nullptr;
+    // }
 
     // - Note
     //      Examples for memory management customization.
     // void operator delete(void *_ptr, size_t _size) noexcept
-    //{
-    //    return __mgmt.deallocate(static_cast<char *>(_ptr), _size);
-    //}
+    // {
+    //   //  return __mgmt.deallocate(static_cast<char *>(_ptr), _size);
+    // }
   };
 
 public:
-  unplug(const promise_type &) noexcept {};
+  unplug(const promise_type &) noexcept {}
 };
 
 } // namespace magic
--- test/test.entry.cpp
+++ test/test.entry.cpp
@@ -9,6 +9,9 @@
 // ---------------------------------------------------------------------------
 #include <magic/coroutine.hpp>
 
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+
 auto bypass() -> magic::unplug
 {
     co_await std::experimental::suspend_never{};
