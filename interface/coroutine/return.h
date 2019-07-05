74daa3cf01c63c042bb2ce59bc1e22f5961a8118
luncliff <luncliff@gmail.com>
2019-07-05 14:56:41 +0900

    vcxproj import and header isolation

--- interface/coroutine/return.h
+++ interface/coroutine/return.h
@@ -8,10 +8,18 @@
 //
 // ---------------------------------------------------------------------------
 #pragma once
-#ifndef COROUTINE_RETURN_TYPES_H
-#define COROUTINE_RETURN_TYPES_H
+#ifndef LUNCLIFF_COROUTINE_RETURN_TYPES_H
+#define LUNCLIFF_COROUTINE_RETURN_TYPES_H
 
+// Try the best if this header file is isolated
+#if __has_include(<coroutine/frame.h>)
 #include <coroutine/frame.h>
+#elif __has_include(<experimental/coroutine>)
+#include <experimental/coroutine>
+#else
+// nothing we can do... :(
+#error "expect header <experimental/coroutine> or <coroutine/frame.h>"
+#endif
 #include <exception>
 
 namespace coro {
@@ -105,4 +113,4 @@ static_assert(sizeof(frame) == sizeof(coroutine_handle<void>));
 
 } // namespace coro
 
-#endif // COROUTINE_RETURN_TYPES_H
+#endif // LUNCLIFF_COROUTINE_RETURN_TYPES_H
