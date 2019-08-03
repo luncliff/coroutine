a66e03411fb4ada52d2dac5281ae04a3d3000a26
luncliff <luncliff@gmail.com>
2019-08-04 02:43:28 +0900

    azure: clang-cl path issue
    * batch uses CXX instead of CMAKE_CXX_COMPILER

--- scripts/build-vs2019-cmake-clang.bat
+++ scripts/build-vs2019-cmake-clang.bat
@@ -2,8 +2,6 @@
 REM The batch script emulates the 'Visual Studio Native Command Prompt'
 REM If the script fails, check the location of 'vcvarsall.bat' and invoke it
 
-set CXX=clang-cl
-
 REM Visual Studio Installer
 REM     Enterprise/Professional/Community
 set vcvarsall_path="C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat"
@@ -19,6 +17,13 @@ if exist %vcvarsall_path% (
     call %vcvarsall_path% x86_amd64
 )
 
-cmake .. -G "Ninja" -DCMAKE_INSTALL_PREFIX="./install" -DBUILD_SHARED_LIBS="%SHARED%" -DCMAKE_BUILD_TYPE="%BUILD_TYPE%" -DCMAKE_CXX_COMPILER="clang-cl"
+path "C:/Program Files/LLVM/bin/";%PATH%
+clang-cl --version
+
+REM We can specify with -DCMAKE_CXX_COMPILER=clang-cl
+REM But for this script we will use CXX variable
+set CXX=clang-cl
+
+cmake . -G "Ninja" -DCMAKE_INSTALL_PREFIX="./install" -DBUILD_SHARED_LIBS="%SHARED%" -DCMAKE_BUILD_TYPE="%BUILD_TYPE%"
 cmake --build .
 cmake --build . --target install
