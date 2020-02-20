bc88a07a5af0d5b4230877ff9e86e843270bc6d4
Park DongHa <luncliff@gmail.com>
2020-02-21 05:02:02 +0900

    test: recover the other code for `channel`

---
+++ test/channel_select_empty.cpp
@@ -0,0 +1,34 @@
+/**
+ * @author github.com/luncliff (luncliff@gmail.com)
+ */
+#undef NDEBUG
+#include <cassert>
+
+#include "internal/channel.hpp"
+#include <coroutine/return.h> // includes `coroutine_traits<void, ...>`
+
+using namespace std;
+using namespace coro;
+
+using u32_chan_t = channel<uint32_t>;
+using i32_chan_t = channel<int32_t>;
+
+int main(int, char*[]) {
+    u32_chan_t ch1{};
+    i32_chan_t ch2{};
+
+    select(
+        ch1,
+        [](auto v) {
+            static_assert(is_same_v<decltype(v), uint32_t>);
+            fputs("select on empty channel must bypass", stderr);
+            exit(__LINE__);
+        },
+        ch2,
+        [](auto v) {
+            static_assert(is_same_v<decltype(v), int32_t>);
+            fputs("select on empty channel must bypass", stderr);
+            exit(__LINE__);
+        });
+    return EXIT_SUCCESS;
+}
