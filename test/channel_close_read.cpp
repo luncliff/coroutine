3402f00b38d73c4c7db605d6f6cc99bf6d09782a
Park DongHa <luncliff@gmail.com>
2020-02-21 04:47:09 +0900

    test: recover some basic tests for `channel`

---
+++ test/channel_close_read.cpp
@@ -0,0 +1,37 @@
+/**
+ * @author github.com/luncliff (luncliff@gmail.com)
+ */
+#undef NDEBUG
+#include <cassert>
+
+#include "internal/channel.hpp"
+#include <coroutine/return.h> // includes `coroutine_traits<void, ...>`
+
+using namespace std;
+using namespace coro;
+
+using channel_without_lock_t = channel<int>;
+
+int main(int, char*[]) {
+    auto ch = make_unique<channel_without_lock_t>();
+    bool ok = true;
+
+    auto coro_read = [&ok](auto& ch, auto& value) -> frame_t {
+        tie(value, ok) = co_await ch.read();
+    };
+
+    auto item = int{};
+    // coroutine will suspend and wait in the channel
+    auto h = coro_read(*ch, item);
+    {
+        auto truncator = move(ch); // if channel is destroyed ...
+    }
+    assert(ch.get() == nullptr);
+
+    coroutine_handle<void>& coro = h;
+    assert(coro.done()); // coroutine is in done state
+    coro.destroy();      // destroy to prevent leak
+
+    assert(ok == false); // and channel returned false
+    return EXIT_SUCCESS;
+}
---
+++ test/channel_close_write.cpp
@@ -0,0 +1,35 @@
+/**
+ * @author github.com/luncliff (luncliff@gmail.com)
+ */
+#undef NDEBUG
+#include <cassert>
+
+#include "internal/channel.hpp"
+#include <coroutine/return.h> // includes `coroutine_traits<void, ...>`
+
+using namespace std;
+using namespace coro;
+
+using channel_without_lock_t = channel<int>;
+
+int main(int, char*[]) {
+    auto ch = make_unique<channel_without_lock_t>();
+    bool ok = true;
+
+    auto coro_write = [&ok](auto& ch, auto value) -> frame_t {
+        ok = co_await ch.write(value);
+    };
+    // coroutine will suspend and wait in the channel
+    auto h = coro_write(*ch, int{});
+    {
+        auto truncator = move(ch); // if channel is destroyed ...
+    }
+    assert(ch.get() == nullptr);
+
+    coroutine_handle<void>& coro = h;
+    assert(coro.done()); // coroutine is in done state
+    coro.destroy();      // destroy to prevent leak
+
+    assert(ok == false); // and channel returned false
+    return EXIT_SUCCESS;
+}
