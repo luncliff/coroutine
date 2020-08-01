56d578009936a6aff09fc2e667d503482bc49f0f
Park DongHa <luncliff@gmail.com>
2020-08-01 20:27:36 +0900

    windows clang: use msvc style compile option
    * misc fixes in CMake and batch script

--- scripts/build-vs2017-cmake-clang.bat
+++ scripts/build-vs2017-cmake-clang.bat
@@ -1,4 +1,4 @@
-
+@echo off
 REM The batch script emulates the 'Visual Studio Native Command Prompt'
 REM If the script fails, check the location of 'vcvarsall.bat' and invoke it
 
@@ -31,6 +31,10 @@ REM We can specify with -DCMAKE_CXX_COMPILER=clang-cl
 REM But for this script we will use CXX variable
 set CXX=clang-cl
 
-cmake . -G "Ninja" -DCMAKE_INSTALL_PREFIX="./install" -DBUILD_SHARED_LIBS="%SHARED%" -DCMAKE_BUILD_TYPE="%BUILD_TYPE%"
+md build
+md install
+cd build
+cmake .. -G "Ninja" -DCMAKE_INSTALL_PREFIX="./install" -DBUILD_SHARED_LIBS="%SHARED%" -DCMAKE_BUILD_TYPE="%BUILD_TYPE%"
 cmake --build .
 cmake --build . --target install
+cd ..
